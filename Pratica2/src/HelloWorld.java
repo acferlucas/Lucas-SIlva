05eb92ca995ed32a9599be540523f290bd5ec200
acferlucas <lafs@discente.ifpe.edu.br>
2021-12-02 23:38:32 -0300

    Novo arquivo de hello world criado

--- Pratica2/src/HelloWorld.java
+++ Pratica2/src/HelloWorld.java
@@ -2,6 +2,8 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Olá mundo, lucas silva");
-		System.out.println("Arquivo da pratica 02");	
+		System.out.println("Arquivo da pratica 02");
+		
+		System.out.println("Nova alteração no arquivo");
 	}
 }
